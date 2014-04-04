c9bdd69b51e0eedda03b2faff1ffbca32413b7c1
jbandener <j.bandener@gmail.com>
2014-04-04 14:24:10 +0200

    changes

--- atmega48_test/atmega48_test.ino
+++ atmega48_test/atmega48_test.ino
@@ -14,16 +14,64 @@
 #include <WProgram.h>
 #include <avr/power.h>
 
+#include <avr/io.h>
+#include <stdlib.h>
+#include <avr/sfr_defs.h>
+//#include <avr/deprecated.h>
+
+#define sbi(ADDRESS,BIT) (ADDRESS |= (1<<BIT))
+#define cbi(ADDRESS,BIT) (ADDRESS &= ~(1<<BIT))
+#define outp(VAL,ADRESS) ADRESS=VAL
+#define inp(VAL) VAL
+
 #endif
 
-int led = 22;
+int led = 21;
+int inValue = 0;
+
+int ADC0read(void) {
+    int inValue = 0;
+    ADMUX = ((0<<REFS1)|(1<<REFS0)|0x00);  // select internal AVCC ref and CHANNEL 0 for ADC
+    ADCSRA = ((1<<ADEN)|(1<<ADIF)|(1<<ADIE)|(1<<ADPS2)|(1<<ADPS1)|(0<<ADPS0));  // select pre scale
+    ADCSRB &= ~((1<<ADTS2)|(1<<ADTS1)|(1<<ADTS0)); // select trigger source as continous
+    ADCSRA |= (1<<ADSC);  // start conversion
+    while((ADCSRA&ADSC)!=ADSC) {   
+    }
+    inValue = ADCL;
+    inValue = inValue + (ADCH<<8);
+    return inValue;
+}
 
 
+void pwm21(int val) {
+  if (val == 0)
+  {
+     cbi(TCCR0A, COM0B1);
+     pinMode(21,OUTPUT);
+     digitalWrite(21, LOW);
+  }
+  else if (val == 255)
+  {
+    cbi(TCCR0A, COM0B1);
+    pinMode(21,OUTPUT);
+    digitalWrite(21, HIGH);
+  }
+  else
+  {
+    sbi(TCCR0A, COM0B1);
+    OCR0B = val;
+  }	
+}
+
+
+int tmp=0;
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);     
+  pinMode(led, OUTPUT); 
+  pinMode(8,INPUT);  
   clock_prescale_set(clock_div_1);  // set frequency back to 8MHz (8MHz / div1)
+  delay(50);
 }
 
 // the loop routine runs over and over again forever:
@@ -32,5 +80,8 @@ void loop() {
   //delay(1);               // wait for a second
   //digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   //delay(1);               // wait for a second
-  analogWrite(led,100);
+  //analogWrite(led,100);
+  tmp = ADC0read();
+  if(tmp<=650)  pwm21(0);
+  else pwm21(255);
 }
--- bubble/bubble.ino
+++ bubble/bubble.ino
@@ -14,6 +14,16 @@ CHANGELOG:
 #include <avr/io.h>
 #include <util/delay.h>
 
+#include <avr/power.h>
+
+
+#define sbi(ADDRESS,BIT) (ADDRESS |= (1<<BIT))
+#define cbi(ADDRESS,BIT) (ADDRESS &= ~(1<<BIT))
+#define outp(VAL,ADRESS) ADRESS=VAL
+#define inp(VAL) VAL
+
+
+
 /*
 PIN DEFINITIONS
 */
@@ -28,7 +38,7 @@ PIN DEFINITIONS
 // FOR ATMEGA48
 int lightSensor = 8;
 int indicatorLED = 5;
-int pwm_out = 3
+int pwm_out = 21
 ;
 int in_pin1 = 17;
 int in_pin2 = 18;
@@ -39,7 +49,7 @@ int reedSensor = 7;
 /*
 CONSTANT DEFINITIONS
 */
-int pwm_speed = 120;
+int pwm_speed = 150;
 int lightThreshold = 950;
 int reedDelay = 10;
 int initialDelay = 600;
@@ -63,8 +73,49 @@ Initialize photo barrier for detecting start and stop
 //void init_light_barrier() {
 //}
 
+/*
+set compare register for OC0B (pin 21 on atmega48)
+*/
+void pwm21(int val) {
+  if (val == 0)
+  {
+     cbi(TCCR0A, COM0B1);
+     pinMode(21,OUTPUT);
+     digitalWrite(21, LOW);
+  }
+  else if (val == 255)
+  {
+    cbi(TCCR0A, COM0B1);
+    pinMode(21,OUTPUT);
+    digitalWrite(21, HIGH);
+  }
+  else
+  {
+    sbi(TCCR0A, COM0B1);
+    OCR0B = val;
+  }	
+}
+
+/*
+reading out ADC0 
+*/
+int ADC0read(void) {
+    int inValue = 0;
+    ADMUX = ((0<<REFS1)|(1<<REFS0)|0x00);  // select internal AVCC ref and CHANNEL 0 for ADC
+    ADCSRA = ((1<<ADEN)|(1<<ADIF)|(1<<ADIE)|(1<<ADPS2)|(1<<ADPS1)|(0<<ADPS0));  // select pre scale
+    ADCSRB &= ~((1<<ADTS2)|(1<<ADTS1)|(1<<ADTS0)); // select trigger source as continous
+    ADCSRA |= (1<<ADSC);  // start conversion
+    delay(10);
+    while((ADCSRA&ADSC)!=ADSC) {   
+    }
+    inValue = ADCL;
+    inValue = inValue + (ADCH<<8);
+    return inValue;
+}
+
 void read_sensor() {
-  sensorValue = analogRead(lightSensor);
+  //sensorValue = analogRead(lightSensor);
+  sensorValue = ADC0read();
 //  sensorValue = digitalRead(reedSensor);
 //  Serial.println(sensorValue);
 //  Serial.println(analogRead(lightSensor));
@@ -76,26 +127,30 @@ Man setup and pin mapping
 void setup() {
 //    Serial.begin(9600);  
 //    init_light_barrier();
+    clock_prescale_set(clock_div_1);  // set frequency back to 8MHz (8MHz / div1) // only used for atmega48 standalone
     pinMode(indicatorLED, OUTPUT);
+    pinMode(lightSensor, INPUT);
     digitalWrite(indicatorLED, LOW);
     pinMode(pwm_out, OUTPUT);
-    analogWrite(pwm_out,0);
+    pwm21(0);
     pinMode(in_pin1, INPUT);
     pinMode(in_pin2, INPUT);    
-    pinMode(reedSensor, INPUT);  
+    //pinMode(reedSensor, INPUT);  
 }
 
 void rotatePart() {
     read_sensor();
     if(valid == 1) {
       digitalWrite(indicatorLED, HIGH);
-      analogWrite(pwm_out, pwm_speed);
+      //analogWrite(pwm_out, pwm_speed);
+      pwm21(pwm_speed);
       
       //if (sensorValue == 1) {
         if (sensorValue <= 650) {
         //delay(reedDelay);
         digitalWrite(indicatorLED, LOW);
-        analogWrite(pwm_out, 0);
+        //analogWrite(pwm_out, 0);
+        pwm21(0);
         valid = 0;
       }
     }
@@ -105,7 +160,8 @@ void rotatePart() {
 
 void initial_rotation(){
   digitalWrite(indicatorLED, HIGH);
-  analogWrite(pwm_out, pwm_speed);
+  //analogWrite(pwm_out, pwm_speed);
+  pwm21(pwm_speed);
   delay(initialDelay);
 }
 
